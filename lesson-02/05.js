017e59b143f36ea61450dccca1569d0c82f0b039
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2023-10-04 21:18:25 +0200

    fifth task

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,9 +1,9 @@
 let passport = {
     name: "Petr",
-    surname: "Petrov"
+    surname: "Petrov",
 }
 
-let passport2 = {...passport};
+let passport2 = {...passport };
 passport2.name = "Ivan";
-console.log(passport);
-console.log(passport2);
+console.log(passport.name);
+console.log(passport2.name);
