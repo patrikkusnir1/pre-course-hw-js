001ea22c1ba360c0f07daa47a0ac8a311678de52
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2023-10-04 20:04:28 +0200

    third task

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,11 +1,11 @@
 // Передай такие числа, что бы у нас выдало надпись 'Something wrong!'
 
 
-const giveMeRightNumbers = (value1, value2,maximum) => {
+const giveMeRightNumbers = (value1, value2, maximum) => {
     if (value1 === value2 || value1 <= maximum) {
         console.log("Success!")
     } else {
-        console.log("Something wrong")
+        console.log("Something wrong!")
     }
 }
 
