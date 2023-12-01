4af59091daad6c4e1ee9696f42fce1277496e145
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-12-01 17:30:58 +0300

    added 7th task

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let passportMarried = {
     name: "Petr",
     surname: "Petrov",
@@ -12,24 +11,5 @@ let passportMarried2 = {...passportMarried, married: true, address: {...passport
 
 console.log(passportMarried)
 console.log(passportMarried2)
-=======
-let passportMarried = {
-    name: "Petr",
-    surname: "Petrov",
-    address: {
-        country: "USA",
-        city: "Bobryisk"
-    }
-};
 
 
-let passportMarried2 = {
-    address: { ...passportMarried.address },
-    married: true,
-    name: passportMarried.name,
-    surname: passportMarried.surname
-};
-
-console.log(passportMarried);
-console.log(passportMarried2);
->>>>>>> 713ca6b536bca1d88aa6318c1683ecd1bd9051ef
